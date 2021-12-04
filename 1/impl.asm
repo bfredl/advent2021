ebaa92c9c5ceba076556880fa560e91049f52021
Björn Linse <bjorn.linse@gmail.com>
2021-12-04 13:27:00 +0100

    test

--- 1/impl.asm
+++ 1/impl.asm
@@ -9,24 +9,42 @@
 
           section   .text
 main:
-          mov  rdi, scanfmt            ; rdi: first arg
-          xor  rax, rax
           sub  rsp,  24
-          lea  rsi, [rsp+8]            ; rsi: second arg
 
+          xor rax, rax
+          mov [rsp+24], rax
+
+          mov  rdi, scanfmt            ; rdi: first arg
+          lea  rsi, [rsp+8]            ; rsi: second arg
           call scanf
 
+loop:
+          mov  rdi, scanfmt            ; rdi: first arg
+          lea  rsi, [rsp+16]           ; rsi: second arg
+          call scanf
+          cmp rax, 1
+          jne done
+
+          mov rcx, [rsp+16]
+          cmp rcx, [rsp+8]
+          jng skip
+          add qword [rsp+24], 1 ; add one if greater
+skip:
+          mov [rsp+8], rcx
+          jmp loop
+
+done:
           mov  rdi, message            ; rdi: first arg
           call puts
 
           mov  rdi, fmt            ; rdi: first arg
-          mov  rsi, [rsp+8]
+          mov  rsi, [rsp+24]
           call printf
           add  rsp, 24
           ret
 message:
           db        "HALLOJ!", 0
 scanfmt:
-          db        "%d", 0
+          db        "%d\n", 0
 fmt:
           db        "%d", 0xA, 0
