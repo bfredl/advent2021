86df56dc74a719fc35757b5fecb96dd9b819bd9f
Björn Linse <bjorn.linse@gmail.com>
2021-12-06 16:19:32 +0100

    onefish

--- 6/impl.asm
+++ 6/impl.asm
@@ -24,6 +24,11 @@ main:
     sub rsp, 6*8
     lea rbx, [rsp+8]; int rbx[9]
 
+    mov rdi, rbx
+    mov rax, 0
+    mov rcx, (9*4)
+    rep stosb
+
 readitem:
     mov rdi, itemline
     lea rsi, [rsp+0]
@@ -36,13 +41,44 @@ readitem:
     jmp readitem
 
 enda:
+
+    xor r13, r13 ; first day is zero
+    xor r14, r14 ; first day is zero
+
+newday:
+    mov eax, [rbx+4*r13]
+    lea rdi, [r13-2]; seven days later = -2 mod 9
+    lea rsi, [r13+7]
+    cmp rdi, 0
+    cmovl rdi, rsi
+    add [rbx+4*rdi], eax
+
+    inc r13
+    xor rax, rax
+    cmp r13, 9
+    cmove r13, rax
+
+    inc r14
+
+    ;printnum r14
+    ;printnum r13
+
+    cmp r14, 160
+    jl newday
+
     xor r12, r12
+    xor r13, r13
 ploop:
-    printnum r12
-    printenum [rbx+4*r12]
+    ;printnum r12
+    mov eax, [rbx+4*r12]
+    add r13, rax
+
+    ;printenum eax
     inc r12
     cmp r12, 9
     jl ploop
+
+    printnum r13
 
     mov rsp, rbp
     pop rbp
